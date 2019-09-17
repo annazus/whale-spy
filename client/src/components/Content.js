4b0da59a868adb4459eaf4f27dccc3df6ff989eb
annazus <suzanna@appliedar.us>
2019-09-17 09:28:56 -0700

    check login status on loading app

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { makeStyles } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -15,13 +15,39 @@ import FilterWindow from "./Filter";
 import NavigationSideBar from "./NavigationSideBar";
 import FullScreenProgressIndicator from "./ProgressIndicator";
 import useGoogleAuth from "../GoogleLogin/GoogleAuth";
+import { getClient } from "../graphql/client";
+import { QUERY_ME } from "../graphql/definitions/queries";
+import { actionTypes } from "../actions";
 const useStyles = makeStyles(theme => ({
   container: { position: "relative", padding: 0 }
 }));
 const Content = () => {
   const classes = useStyles();
-  const { state } = useContext(Context);
+  const { state, dispatch } = useContext(Context);
   const { isSignedIn, googleUser } = useGoogleAuth();
+  useEffect(() => {
+    let idToken, client;
+    if (googleUser) {
+      idToken = googleUser.idToken;
+      client = getClient(idToken);
+    }
+
+    const login = async () => {
+      try {
+        const result = await client.query({ query: QUERY_ME });
+        console.log("me", result.data.me);
+        dispatch({
+          type: actionTypes.LOGIN_USER,
+          payload: { user: result.data.me }
+        });
+      } catch (error) {
+        console.log("error", error);
+        //the person does not have an accout with this email - redirect to sign up
+      }
+    };
+    if (idToken) login();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [googleUser]);
   console.log("googleUser", googleUser);
   return (
     <>
