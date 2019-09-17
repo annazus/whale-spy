0641d4c77d481407c1abf9e9f70191d761a2f1d3
annazus <suzanna@appliedar.us>
2019-09-17 14:18:48 -0700

    update grapqhl urls for production

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -8,18 +8,22 @@ import { createHttpLink } from "apollo-link-http";
 import { split } from "apollo-link";
 import { getMainDefinition } from "apollo-utilities";
 
-export const GRAPHQL_SERVER_URL = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`;
-// export const GRAPHQL_SERVER_URL = `https://${process.env.REACT_APP_HOST}`;
-export const GRAPHQL_SUBSERVER_URL = `ws://${process.env.REACT_APP_HOST}:4000/graphql`;
+let GRAPHQL_SERVER_URL;
+let GRAPHQL_SUBSERVER_URL;
 
-// export const GRAPHQL_SUBSERVER_URL = `wss://${
-//   process.env.REACT_APP_HOST
-// }/graphql`;
+process.env.NODE_ENV === "production"
+  ? (GRAPHQL_SERVER_URL = `https://${process.env.REACT_APP_HOST}`)
+  : (GRAPHQL_SERVER_URL = `http://${process.env.REACT_APP_HOST}:${process.env.REACT_APP_PORT}`);
+
+process.env.NODE_ENV === "production"
+  ? (GRAPHQL_SUBSERVER_URL = `wss://${process.env.REACT_APP_HOST}/graphql`)
+  : (GRAPHQL_SUBSERVER_URL = `ws://${process.env.REACT_APP_HOST}:4000/graphql`);
 
 const httpLink = new createHttpLink({
   uri: GRAPHQL_SERVER_URL // use https for secure endpoint
 });
 
+export { GRAPHQL_SERVER_URL, GRAPHQL_SUBSERVER_URL };
 const wsLink = new WebSocketLink({
   uri: GRAPHQL_SUBSERVER_URL,
   options: {
